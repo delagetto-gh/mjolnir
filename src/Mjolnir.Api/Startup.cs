517dbf1b46f06d53c3b152c46aa84dc39005a394
delagetto <noreply@delagetto.com>
2022-03-07 22:47:38 -0500

    set-up jwt scheme auth in Mjolnir api

--- src/Mjolnir.Api/Startup.cs
+++ src/Mjolnir.Api/Startup.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Mjolnir.Api.Options;
 
 namespace Mjolnir.Api
@@ -27,7 +32,32 @@ namespace Mjolnir.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.Configure<BifrostOptions>(Configuration.GetSection(BifrostOptions.Key));
+            services.Configure<BifrostConfiguration>(Configuration.GetSection(BifrostConfiguration.Key));
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme) //use authentication services (use JwtBearer by default/fallback)
+                    .AddJwtBearer(options => //add the 'jwt bearer' scheme -  scheme is a name which corresponds to an authentication handler (+ its options)
+                    {
+                        var bifrostConfig = Configuration
+                            .GetSection(BifrostConfiguration.Key)
+                            .Get<BifrostConfiguration>();
+
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnChallenge = ctx =>
+                            {
+                                ctx.HandleResponse();
+                                ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
+                                ctx.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Unowrthy";
+                                return Task.CompletedTask;
+                            }
+                        };
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            IssuerSigningKey = new SymmetricSecurityKey(GetBytes(bifrostConfig.Secret)),
+                            ClockSkew = TimeSpan.Zero,
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+                        };
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +72,8 @@ namespace Mjolnir.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -49,5 +81,6 @@ namespace Mjolnir.Api
                 endpoints.MapControllers();
             });
         }
+        private byte[] GetBytes(string secret) => Encoding.UTF8.GetBytes(secret);
     }
 }
