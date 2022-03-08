d113e2f52b7c24cac81fc4380546c5c9aef818d9
delagetto <noreply@delagetto.com>
2022-03-07 22:51:08 -0500

    refactor from options to configuration (+namespace change)

---
+++ src/Mjolnir.Api/Configurations/BifrostConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Mjolnir.Api.Configurations
+{
+    public class BifrostConfiguration
+    {
+        public const string Key = "Bifrost";
+        public string Secret { get; set; }
+    }
+}
--- src/Mjolnir.Api/Options/BifrostOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Mjolnir.Api.Options
-{
-    public class BifrostOptions
-    {
-        public const string Key = "Bifrost";
-        public string Secret { get; set; }
-        public string SecurityAlgorithm { get; set; }
-    }
-}
