95b2d0e92ced6387132292b6d88a8ffe9042b33c
delagetto <noreply@delagetto.com>
2022-03-14 23:28:18 -0400

    clearing the way before i start adding asp.net core identity

---
+++ src/Services/Heimdall/Configurations/HeimdallConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Heimdall.Configurations
+{
+    public class HeimdallConfiguration
+    {
+        public const string Key = "Heimdall";
+        public string Secret { get; set; }
+    }
+}
--- src/Services/Heimdall/Startup.cs
+++ src/Services/Heimdall/Startup.cs
@@ -1,21 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Heimdall.Options;
-using Heimdall.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-
 namespace Heimdall
 {
     public class Startup
@@ -31,27 +18,6 @@ namespace Heimdall
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            //add authentiation services
-            services
-            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(opt =>
-            {
-                var jwtOpts = new JwtOptions();
-                Configuration.Bind(JwtOptions.Key, jwtOpts);
-                var signingKey = Encoding.ASCII.GetBytes(jwtOpts.Secret);
-
-                opt.SaveToken = true;
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(signingKey),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-            services.Configure<JwtOptions>(Configuration.GetSection(JwtOptions.Key));
-            services.AddScoped<JwtAuthenticationManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,8 +31,6 @@ namespace Heimdall
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthentication(); //add authentication middleware 
 
             app.UseAuthorization();
 
