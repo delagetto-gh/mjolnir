bab1afc54f20f1160477f2cdc6dfa1ef8aa479d6
delagetto <noreply@delagetto.com>
2022-03-19 10:04:40 -0400

    updates stub name to be more explicit

---
+++ tests/Services/Heimdall/Unit/Doubles/UserManagerPreExistingUserStub.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace Unit.Doubles
+{
+    internal class UserManagerPreExistingUserStub<TUser> : UserManager<TUser> where TUser : class, new()
+    {
+        public UserManagerPreExistingUserStub(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators, IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<TUser>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+        }
+
+        public override Task<TUser> FindByNameAsync(string userName)
+        {
+            return Task.FromResult(new TUser());
+        }
+    }
+}
